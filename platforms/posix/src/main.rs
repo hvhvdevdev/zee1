da3ba03a52d7963982d6a01840d348e853d5e2c0
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-08-23 11:06:34 +0700

    comments

--- platforms/posix/src/main.rs
+++ platforms/posix/src/main.rs
@@ -2,8 +2,11 @@ use platform_posix::config_reader::CmdLineConfigReader;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
+    // Read the run configuration from command line arguments.
     let config = CmdLineConfigReader::new(std::env::args().collect())
         .read()
         .unwrap();
+
+    // Print the run configuration for debugging purposes.
     println!("{:#?}", config);
 }
